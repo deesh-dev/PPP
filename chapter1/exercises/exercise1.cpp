4171ac4b56c49b2f80a1f34030263b7e21eb52fd
Deepak Kumar <workforudeepak@gmail.com>
2025-10-18 15:01:34 +0530

    Chapter 1

---
+++ chapter1/exercises/exercise1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(void)
+{
+  std::cout << "Hello, programming!\n";
+  std::cout << "Here we go!\n";
+  return 0;
+}
---
+++ chapter1/terms/terms.cpp
@@ -0,0 +1,36 @@
+// Executable: The file which gets outputted after comipiling and linking and is in machine code.
+
+// main(): Main is the function which every C++ program should contain. The main() function is where the execution of our programs start.
+
+// << : This is the output stream sign used to put a value in something, for example in std::cout << "Hello, World!"; .
+
+// function: A function is like a container which necessarily has a return type(int, bool, etc), optionally can take parameters and has a body of lines of code which do a task and a function may or may not have a side-effect like printing to the console. Functions are useful so that we can seperate chunks of logic into different parts of our program which makes our code solid and highly maintainable as we don't have to change everything in main when we introduce new features.
+
+// Object code: Object code is basically source code- which is the code we humans write in programming languages like C++, Python, JS and more.
+
+// Header file: A header file like iostream contains standard c++ functionality for input and output. Other header files may contain different functions to achieve other things. These header files are mostly to let the programmer stand on the shoulders of others before them and use their code without reinventing the wheel and lets them focus on their current problems.
+
+// Output: Output is anything which the program produces- a data file or any visual sideffect.
+
+// Comment: Comments are used for the programmer and their team to document exactly what each part of the code does- which helps them and others working on the codebase to maintain it.
+
+// IDE: IDE stands for Integrated Development Environment- it's basically an environment which gives you every tool like compiler, debugger and others in a place- so programmers can focus on writing code. Popular IDE for C++ is CLion and VSCode with extensions.
+
+// Program: A program is a set of instructions in any programming language like C++ for the computer to do.
+
+// Compiler: A compiler is a program which checks whether the object/source code convenes to the standard laid out by the language syntax and if yes- then transforms the object code to machine code.
+
+// Source code: Source is object code- the code written by humans in a programming language like C++.
+
+// Compile-time error: Compile-time errors are errors which the compiler issues to the programmer which basically hints what might be wrong with a program's syntax or likewise and halts the compilation till it's fixed. 
+
+// Statement: A statement in C++ is a complete instruction for the computer to do like std::cout << "Hello"; and a statement ends with a semi-colon
+
+// cout: Cout is used to output something to the console and it's part of the standard library iostream.
+
+// Linker: Linker is a program which tries to link libraries used in a program like iostream and then it outputs the final machine code if there are no link-time errors.
+
+// Bug: A bug is any kind of mistake logically which doesn't let the program behave as the programmer intended to in runtime.
+
+// Debugging: Debugging is the process of fixing bugs, so as to make the program behave as intended.
+
