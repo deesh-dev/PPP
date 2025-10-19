aa46a2c3769bdc4cc039b43e721c4d42d4273558
Deepak Kumar <workforudeepak@gmail.com>
2025-10-19 19:51:18 +0530

    Exercise sixth question

---
+++ chapter2/exercises/main6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main(void){
+    std::cout << "Enter three integer values: ";
+    std::vector <int> nums;
+    int x{};
+    int y{};
+    int z{};
+    std::cin >> x >> y >> z;
+    nums.push_back(x);
+    nums.push_back(y);
+    nums.push_back(z);
+
+    std::sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size(); i++) std::cout << nums[i] ;
+    std::cout << '\n';
+}
