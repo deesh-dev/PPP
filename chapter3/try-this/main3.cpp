902664a436ebced96032466d8c2a62c7e1ccaaf5
Deepak Kumar <workforudeepak@gmail.com>
2025-10-20 12:41:39 +0530

    Try-this number 4

--- chapter3/try-this/main3.cpp
+++ chapter3/try-this/main3.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
 int main(void){
-    for (int i = 0; i < 26; i++){
-        int value_of_a = 97;
-        std::cout << char(value_of_a + i) << '\t' << int('a' + i) << '\n'; 
+    int i = 0;
+    while (i < 26){
+        std::cout << char('a' + i) << '\t' << int('a' + i) << '\n'; 
+        i++;
     }
     return 0;
 }
