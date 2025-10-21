38affcb3f110743d7d2973763aa9a7e9a180778a
Deepak Kumar <workforudeepak@gmail.com>
2025-10-21 09:32:35 +0530

    Drill 2

---
+++ chapter3/drills/main2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(void){
+    int num1{};
+    int num2{};
+    while(std::cin >> num1 >> num2){
+        if (num1 > num2){
+            std::cout << "The smaller value is: " << num2 << '\n';
+            std::cout << "The larger value is: " << num1 << '\n';
+        }
+        else{
+            std::cout << "The smaller value is: " << num1 << '\n';
+            std::cout << "The larger value is: " << num2 << '\n';
+        }
+    }
+    return 0;
+}
