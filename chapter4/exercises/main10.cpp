298270e7c1066ab20c4ae6443c74c3fac98e75cb
Deepak Kumar <workforudeepak@gmail.com>
2025-10-25 11:31:47 +0530

    Exercise question 10

--- chapter4/exercises/main10.cpp
+++ chapter4/exercises/main10.cpp
@@ -2,14 +2,14 @@
 #include <vector>
 
 int main(void){
-    int sum{0};
-    std::vector <int> numbers{};
+    double sum{0};
+    std::vector <double> numbers{};
     std::cout << "Please enter the number of values you want to sum: ";
     int numValues{0};
     std::cin >> numValues;
 
     std::cout << "Please enter some integers (press '|' to stop): ";
-    for(int num; std::cin >> num;){
+    for(double num; std::cin >> num;){
         std::cout << "Please enter some integers (press '|' to stop): ";
         numbers.push_back(num);
     }
@@ -27,6 +27,14 @@ int main(void){
         std::cout << numbers[i] << " ";
     }
     std::cout << ")" << " is " << sum << '\n';
+
+    std::vector <double> adjancentDifference(numbers.size() - 1);
+    for (int i = 0 ; i < adjancentDifference.size(); i++){
+        adjancentDifference[i] = numbers[i] - numbers[i + 1];
+    } 
+    std::cout << "( ";
+    for (double num : adjancentDifference) std::cout << num << ' ';
+    std::cout << ")" << '\n';
 
     return 0;
 }
